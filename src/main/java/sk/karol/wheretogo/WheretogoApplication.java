7517cd6a890aebdde9456d106ce7b2c3eb470805
coudy23 <kcapay@gmail.com>
2020-06-09 16:55:03 +0200

    Create project

---
+++ src/main/java/sk/karol/wheretogo/WheretogoApplication.java
@@ -0,0 +1,13 @@
+package sk.karol.wheretogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheretogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheretogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/karol/wheretogo/WheretogoApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.karol.wheretogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheretogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
